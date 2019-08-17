6dc4626577506ba0fb9e7caed031e645a8752a19
cspaier <clement.spaier@gmail.com>
2019-08-17 18:09:50 +0200

    on a un plugin fonctionnel

---
+++ thonnycontrib/pydiderot-plugin/__init__.py
@@ -0,0 +1,10 @@
+
+def load_plugin():
+    """
+    Cette fonction sera appellée au lancement de thonny.
+    On peut donc y faire les configurations que l'on veut.
+
+    Todo:
+    - changer le dossier de travail par %HOMESHARE%/python/
+    - ajouter le dossier partager en écriture contenant les libs maisons au sys.path
+    """
